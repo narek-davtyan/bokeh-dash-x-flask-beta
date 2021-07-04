69d835edf50097b3ee59d875da7de878779a102d
Narek Davtyan <davtyanlnarek@gmail.com>
2021-07-04 19:33:36 +0200

    New

--- main.py
+++ main.py
@@ -767,7 +767,7 @@ def bkapp_page():
 def bk_worker():
     asyncio.set_event_loop(asyncio.new_event_loop())
 
-    bokeh_tornado = BokehTornado({'/bkapp': bkapp}, extra_websocket_origins=["localhost:8000"])
+    bokeh_tornado = BokehTornado({'/bkapp': bkapp}, extra_websocket_origins=["localhost:8000", "0.0.0.0:8000", "localhost:5000", "0.0.0.0:5000"])
     bokeh_http = HTTPServer(bokeh_tornado)
     bokeh_http.add_sockets(sockets)
 
